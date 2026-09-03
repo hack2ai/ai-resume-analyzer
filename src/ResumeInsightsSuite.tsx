ab5c04e004dea0784c385c384a967307cf4281dc
Pankaj Kumar <83492104+hack2ai@users.noreply.github.com>
2026-09-03 12:01:05 +0530

    Integrate interview preparation into insights

--- src/ResumeInsightsSuite.tsx
+++ src/ResumeInsightsSuite.tsx
@@ -7,6 +7,7 @@ import SkillGapAnalyzer from "./SkillGapAnalyzer";
 import CareerReadinessScore from "./CareerReadinessScore";
 import ResumeActionPlan from "./ResumeActionPlan";
 import ResumeVersionComparison from "./ResumeVersionComparison";
+import InterviewPreparation from "./InterviewPreparation";
 
 type Analysis={id?:string;analysisId?:string;atsScore:number;matchPercentage:number;missingKeywords:string[];strengths:string[];improvements:string[];summary:string;resumeFileName?:string;resumeText?:string;jobDescription?:string;jobTitle?:string|null;createdAt?:string};
 type Props={analyses:Analysis[];currentAnalysis?:Analysis|null;jobDescription:string;resumeText?:string;dark?:boolean;onView:(analysis:any)=>void;onDownload:(analysis:any)=>void};
@@ -29,6 +30,7 @@ export default function ResumeInsightsSuite({analyses,currentAnalysis,jobDescrip
   <ResumeVersionComparison analyses={analyses} dark={dark}/>
   <CareerReadinessScore analysis={analysis} dark={dark}/>
   <ResumeActionPlan analysis={analysis} dark={dark}/>
+  <InterviewPreparation analysis={analysis} dark={dark}/>
   <AnalysisDetails analysis={analysis} dark={dark}/>
   <CareerRecommendations analysis={analysis} dark={dark}/>
   <ImprovementRoadmap analysis={analysis} dark={dark}/>
